0c493241c4680b85f46b7e34baa1b5aa802901c7
Marcin Leszczyński <33961381+MrDell1@users.noreply.github.com>
2023-05-14 18:57:36 +0200

    Update SessionService.tsx

--- src/services/SessionService.tsx
+++ src/services/SessionService.tsx
@@ -8,7 +8,7 @@ import {
 } from "react";
 
 export const urlBase =
-  "http://ec2-16-16-160-27.eu-north-1.compute.amazonaws.com";
+  "https://toss-a-coin.azurewebsites.net";
 
 export type AuthService = {
   signOut: () => Promise<void>;
